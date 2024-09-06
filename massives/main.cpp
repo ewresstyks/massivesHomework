c8ea5bef15dd4ef0ae7ac786a79729bc409a1d50
ewresstyks <dizzyhate2008@icloud.com>
2024-09-06 17:16:18 +0300

    Initial Commit

---
+++ massives/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  massives
+//
+//  Created by Quals on 06.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
